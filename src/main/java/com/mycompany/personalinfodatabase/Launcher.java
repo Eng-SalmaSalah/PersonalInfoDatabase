b4527a9235621403dd10e99a62943fd225279a63
salma salah mahmoud <40217644+Eng-SalmaSalah@users.noreply.github.com>
2019-04-06 15:51:30 +0200

    Gui created

---
+++ src/main/java/com/mycompany/personalinfodatabase/Launcher.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.personalinfodatabase;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author salma
+ */
+public class Launcher extends Application{
+     public static void main(String[] args) {
+        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        GUI root = new GUI();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+}
