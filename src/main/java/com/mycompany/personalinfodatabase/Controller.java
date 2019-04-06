67a8e8dd8ff283a7e17afd9d914566337c618495
AmrElkady <amrelkady93@gmail.com>
2019-04-06 16:33:17 +0200

    add property file to .ignor and creat controller class

---
+++ src/main/java/com/mycompany/personalinfodatabase/Controller.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.personalinfodatabase;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Amr Elkady
+ */
+public class Controller {
+    
+    
+    
+    
+    DAO dao;
+    Person person;
+
+    ResultSet resultSet;
+    public boolean flag = false;
+
+    public Controller(DAO dao) {
+        this.dao = dao;
+        person = new Person();
+        
+    }
+    
+    
+    //salma
+    
+    
+    
+    
+    //amr
+    
+    
+}
