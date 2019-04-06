f48c105e5bcb9a8c4e305499a8632e60c9e7ed61
salma salah mahmoud <40217644+Eng-SalmaSalah@users.noreply.github.com>
2019-04-06 15:58:04 +0200

    person class created

--- src/main/java/com/mycompany/personalinfodatabase/Person.java
+++ src/main/java/com/mycompany/personalinfodatabase/Person.java
@@ -10,5 +10,69 @@ package com.mycompany.personalinfodatabase;
  * @author salma
  */
 public class Person {
+    private int id ;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    public Person(int id, String firstName, String middleName, String lastName, String email, String phone) {
+        this.id = id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
     
 }
