e8a1ad847a377dccc635e10f6fde3920ef48f37e
AmrElkady <amrelkady93@gmail.com>
2019-04-06 15:30:29 +0200

    add DB dependance and creat Person Bean and connect to DB

---
+++ src/main/java/com/mycompany/personalinfodatabase/DataBaseConnector.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.personalinfodatabase;
+
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+import javax.sql.DataSource;
+
+
+
+/**
+ *
+ * @author Amr Elkady
+ */
+public class DataBaseConnector  {
+    
+    //salma
+    
+    //amr
+     public static DataSource connectToDataBase() {
+
+        //Properties prob = new Properties();
+       // FileInputStream file = null;
+        MysqlDataSource mysqlDataSource = null;
+        Properties properties = new Properties();
+        OutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream("db.properties");
+            properties.setProperty("MYSQL_DB_URL", "jdbc:mysql://localhost:3306/test");
+            properties.setProperty("MYSQL_DB_USERNAME", "root");
+            properties.setProperty("MYSQL_DB_PASSWORD", "root");
+            properties.store(outputStream, null);
+            
+            FileInputStream file = new FileInputStream("db.properties");
+            
+            properties.load(file);
+            
+            mysqlDataSource = new MysqlDataSource();
+
+            mysqlDataSource.setURL(properties.getProperty("MYSQL_DB_URL"));
+            mysqlDataSource.setUser(properties.getProperty("MYSQL_DB_USERNAME"));
+            mysqlDataSource.setPassword(properties.getProperty("MYSQL_DB_PASSWORD"));
+            System.out.println(properties.getProperty("MYSQL_DB_PASSWORD"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return mysqlDataSource;
+    }
+
+    
+}
---
+++ src/main/java/com/mycompany/personalinfodatabase/Person.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.personalinfodatabase;
+
+/**
+ *
+ * @author Amr Elkady
+ */
+public class Person {
+    
+    //salma
+    
+    
+    
+    //amr
+    private int id ;
+    private String fName;
+    private String mName;
+    private String lName;
+    private String email;
+    private String phone;
+
+    public Person(int id, String fName, String mName, String lName, String email, String phone) {
+        this.id = id;
+        this.fName = fName;
+        this.mName = mName;
+        this.lName = lName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Person() {
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+     
+    
+}
