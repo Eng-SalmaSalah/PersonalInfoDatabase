037e3d0d47a2b8db65b84c5a93edbd1fd9843694
AmrElkady <amrelkady93@gmail.com>
2019-04-06 16:40:22 +0200

    last and first and update record

--- src/main/java/com/mycompany/personalinfodatabase/Controller.java
+++ src/main/java/com/mycompany/personalinfodatabase/Controller.java
@@ -6,16 +6,14 @@
 package com.mycompany.personalinfodatabase;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
  * @author Amr Elkady
  */
 public class Controller {
-    
-    
-    
-    
+
     DAO dao;
     Person person;
 
@@ -25,16 +23,63 @@ public class Controller {
     public Controller(DAO dao) {
         this.dao = dao;
         person = new Person();
-        
     }
-    
-    
+
     //salma
-    
-    
-    
-    
     //amr
-    
-    
+    public Person firstRecord() {
+
+        try {
+            resultSet.first();
+            person.setId(resultSet.getInt("id"));
+            person.setfName(resultSet.getString("fname"));
+            person.setlName(resultSet.getString("lname"));
+            person.setmName(resultSet.getString("mname"));
+            person.setEmail(resultSet.getString("email"));
+            person.setPhone(resultSet.getString("phone"));
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return person;
+    }
+
+    public Person lastRecord() {
+
+        try {
+            resultSet.last();
+            person.setId(resultSet.getInt("id"));
+            person.setfName(resultSet.getString("fname"));
+            person.setlName(resultSet.getString("lname"));
+            person.setmName(resultSet.getString("mname"));
+            person.setEmail(resultSet.getString("email"));
+            person.setPhone(resultSet.getString("phone"));
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return person;
+    }
+
+    public void updateRow(Person person) {
+        try {
+            if (!flag) {
+
+                resultSet.updateInt("id", person.getId());
+                resultSet.updateString("fname", person.getfName());
+                resultSet.updateString("lname", person.getlName());
+                resultSet.updateString("mname", person.getmName());
+                resultSet.updateString("email", person.getEmail());
+                resultSet.updateString("phone", person.getPhone());
+                resultSet.updateRow();
+            } else {
+                System.out.println("no Row TO update");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
